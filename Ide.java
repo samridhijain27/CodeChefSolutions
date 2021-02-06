7fca1de477f4e806e27a131c595f8e0c903d5c0e
Samridhi Jain <samridhijain@Samridhis-MacBook-Air.local>
2021-02-07 01:31:34 +0530

    First Commit

---
+++ Ide.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		 if ((N>=1) && (N<=10))
+		 {
+		 System.out.println(N);
+		 }
+		 else
+		 { 
+		     
+		     for(int i = 10; i>=1; i--)
+		     {
+		         //System.out.println(i);
+		         if (N % i == 0)
+		     {
+		         System.out.println(i);
+		         break;
+		     }
+		     
+		     }
+		 }
+	}
+}
